a57ab02be56688fab0947cb7efb61605fb4da38e
Daniel Mateo Patarroyo Molina <danielmateopatarroyomolina@gmail.com>
2023-06-19 18:05:07 -0500

    Final_proyect

---
+++ Final/Ventana.java
@@ -0,0 +1,34 @@
+package Final;
+
+import java.awt.GridLayout;
+import javax.swing.JFrame;
+
+
+public class Ventana extends JFrame {
+    private static final long serialVersionUID = 1L;
+
+    private Panel panel;
+
+    public Ventana() {
+
+        setTitle("Dibujo bruja - Luis Mariño");
+        setSize(400, 300);
+        setVisible(true);
+        setResizable(false);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new GridLayout(1, 1));
+        setLocationRelativeTo(null);
+
+        panel = new Panel();
+
+        add(panel);
+    }
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Ventana().setVisible(true);
+            }
+        });
+    }
+}
